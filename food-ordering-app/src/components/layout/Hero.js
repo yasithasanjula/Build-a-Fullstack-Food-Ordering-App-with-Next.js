a3a7b2e80fca1f2807a98c1a9976b3182b7a1c6b
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-10 22:41:17 +0530

    111

--- food-ordering-app/src/components/layout/Hero.js
+++ food-ordering-app/src/components/layout/Hero.js
@@ -4,22 +4,28 @@ import Right from '../icons/Right';
 
 export default function Hero() {
   return (
-    <section className='grid grid-cols-2'>
-        <div>
+    <section className='hero md:mt-4'>
+        <div className='py-8 md:py-12'>
             <h1 className='text-4xl font-semibold'>
-                Everything is better with a Pizza
+                Everything <br />
+                is better <br /> 
+                with a&nbsp; 
+                <span className='text-primary'>
+                    Pizza
+                </span>
             </h1>
-            <p className='my-4 text-gray-500'>
+            <p className='my-6 text-sm text-gray-500'>
              Pizza is the missing piece that makes every day
              complete, a simple yet delicious joy in life 
             </p>
-            <div className='flex gap-4 '>
+            <div className='flex gap-4 text-sm'>
                 <button className="flex items-center justify-center gap-2 px-4 py-2 text-white uppercase rounded-full bg-primary">
                     Order now 
-                    <Right />
+                    <Right /> 
                 </button>
-                <button>
+                <button className='flex items-center gap-2 py-2 font-semibold text-gray-600 border-0 '>
                     Learn more
+                    <Right />
                 </button>
             </div>
         </div>
