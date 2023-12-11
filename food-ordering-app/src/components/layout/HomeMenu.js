41e6da168096b7bfbb828f6c1844ca323d64a69a
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-11 12:57:33 +0530

    111

--- food-ordering-app/src/components/layout/HomeMenu.js
+++ food-ordering-app/src/components/layout/HomeMenu.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function HomeMenu() {
   return (
     <>
     <section className=''>
+      <div className='absolute left-0 right-0 justify-start w-full h-full'>
+        <div className='absolute left-0 -top-[70px] -z-10'>
+          <Image src={'/sallad1.png'} width={109} height={189} alt={'sallad'}/>
+        </div>
+
+        <div className='absolute right-0 -top-36 -z-10'>
+          <Image src={'/sallad2.png'} width={107} height={195} alt={'sallad'}/>
+        </div>
+        
+      </div>
         <div className='text-center'>
-          <h3 className='font-semibold text-gray-500 uppercase'>
+          <h3 className='font-semibold leading-4 text-gray-500 uppercase'>
             Check out
           </h3>
-          <h2 className='text-4xl font-bold text-primary'>Menu</h2>  
+          <h2 className='text-4xl italic font-bold text-primary'>Menu</h2>  
         </div>
         
     </section>
