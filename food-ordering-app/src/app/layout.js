f8152903a8cbc5bd35a1a34758104acc891e8e06
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-11 23:32:13 +0530

    111

--- food-ordering-app/src/app/layout.js
+++ food-ordering-app/src/app/layout.js
@@ -1,6 +1,7 @@
+import Header from "@/components/layout/Header"
 import { Roboto } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/layout/Header'
+
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
@@ -11,15 +12,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className={roboto.className}>
-        <main className='max-w-4xl p-4 mx-auto'>
-          <Header />
-           {children}
-          <footer className='p-8 mt-16 text-center text-gray-500 border-t'>
-            &copy; 2013 All right reserved
-          </footer>
-      
+        <main className="max-w-4xl p-4 mx-auto">
+          
+            <Header />
+            {children}
+            <footer className="p-8 mt-16 text-center text-gray-500 border-t">
+              &copy; 2023 All rights reserved
+            </footer>
+        
         </main>
       </body>
     </html>
