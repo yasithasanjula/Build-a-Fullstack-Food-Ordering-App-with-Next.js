919657634c047f2dba159d554ef48000f086b514
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-13 19:17:05 +0530

    111

--- food-ordering-app/src/models/User.js
+++ food-ordering-app/src/models/User.js
@@ -1,4 +1,5 @@
 import {model, models, Schema} from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
   
@@ -9,11 +10,17 @@ const UserSchema = new Schema({
     validate: pass => {
       if(!pass?.length || pass.length < 5) {
         new Error("password must be at least 5 characters");
-        return false;
+        
       }
     },
   } ,
   
 }, {timestamps: true});
+
+UserSchema.post('validate', function (user) {
+  const notHashedPassword = user.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(notHashedPassword, salt);
+});
 
 export const User = models?.User || model('User', UserSchema);
