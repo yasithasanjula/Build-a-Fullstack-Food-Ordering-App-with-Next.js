00ed9f9e01d140b93b2e53ff204fa39c0cfd423c
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-14 17:28:40 +0530

    1111

---
+++ food-ordering-app/src/app/profile/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <section className="mt-8">
+        <h1 className="mb-4 text-4xl text-center text-primary">
+            Profile
+        </h1>
+    </section>
+  )
+}
--- food-ordering-app/src/components/layout/Header.js
+++ food-ordering-app/src/components/layout/Header.js
@@ -6,7 +6,12 @@ import Link from 'next/link'
 export default function Header() {
   const session = useSession();
   console.log(session);
-  const status = session.status;
+  const status = session?.status;
+  const userData = session.data?.user;
+  let userName = userData?.name || userData?.email;
+  if(userName && userName.includes(' ')) {
+    userName = userName.split(' ')[0];
+  }
    return(
     <header className='flex items-center justify-between'>
     <nav className='flex items-center gap-8 text-gray-500 text-semibold'>
@@ -20,10 +25,16 @@ export default function Header() {
     </nav>
     <nav className='flex items-center gap-4 text-gray-500 text-semibold'>
       {status === 'authenticated' && (
+        <>
+        <Link href={'/profile'} className='whitespace-nowrap'>
+          Hello, {userName}
+        </Link>
         <button onClick={() => signOut()}
         className='px-8 py-2 text-white rounded-full bg-primary'>
         Logout
        </button>
+        </>
+        
       )}
       {status === 'unauthenticated' && (
         <>
