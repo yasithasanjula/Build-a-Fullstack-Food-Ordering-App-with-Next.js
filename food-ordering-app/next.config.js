0cf067525df876957cf85b64cc2765061744b9aa
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-21 10:11:32 +0530

    111

--- food-ordering-app/next.config.js
+++ food-ordering-app/next.config.js
@@ -1,4 +1,15 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '*.googleusercontent.com',
+          },
+        ]
+}
+}
 
 module.exports = nextConfig
--- food-ordering-app/src/app/profile/page.js
+++ food-ordering-app/src/app/profile/page.js
@@ -1,16 +1,39 @@
 'use client';
 import React from 'react'
 import {useSession} from 'next-auth/react';
+import {redirect} from 'next/navigation';
+import Image from 'next/image';
 
 
 export default function page() {
   const session = useSession();
-  console.log(session);
+  const {status} = session;
+
+  if(status == 'loading'){
+    return 'Loading...'
+  }
+  
+  if(status == 'unauthenticated'){
+    redirect('/login');
+  }
+
+  const userImage = session.data.user.image;
   return (
     <section className="mt-8">
         <h1 className="mb-4 text-4xl text-center text-primary">
             Profile
         </h1>
+        <form className="max-w-md mx-auto">
+          <div className='flex items-center gap-2'>
+            <div>
+              <Image src={userImage} width={80} height={80} alt={'avater'} />
+            </div>
+            <div className='grow'>
+              <input type="text" placeholder='first and last name'/>
+            </div>
+            
+          </div>
+        </form>
     </section>
   )
 }
