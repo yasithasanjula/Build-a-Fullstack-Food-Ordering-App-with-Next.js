379f454b1a5a570e1f1325be72e07bf7e178fd9b
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-12-21 09:39:16 +0530

    111

--- food-ordering-app/src/app/profile/page.js
+++ food-ordering-app/src/app/profile/page.js
@@ -1,6 +1,11 @@
+'use client';
 import React from 'react'
+import {useSession} from 'next-auth/react';
+
 
 export default function page() {
+  const session = useSession();
+  console.log(session);
   return (
     <section className="mt-8">
         <h1 className="mb-4 text-4xl text-center text-primary">
